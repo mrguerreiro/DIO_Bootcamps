423877df175297fb781590a722c2a20f39bd164b
mrguerreiro <marcosguerreiro@hotmail.com>
2022-04-22 19:25:41 -0300

    Inclusao aula Javascript

---
+++ HTML_Web_Developer/aula_javascript/js/main.js
@@ -0,0 +1,91 @@
+
+
+/*
+desafio folha de pagamento
+
+let valor1 = parseInt(gets());
+let valor2 = parseInt(gets());
+let valor3 = parseFloat(gets());
+
+let salario = parseFloat(valor2 * valor3).toFixed(2);
+
+console.log("NUMBER = " + valor1);
+console.log("SALARY = U$ " + salario);
+*/
+
+
+/*
+Desafio multiplicação simples
+
+let valor1 = parseInt(gets(0));
+let valor2 = parseInt(gets(1));
+let total = valor1 * valor2;
+console.log("PROD = " + total);
+*/
+
+
+/* Desafio sacola da feira
+let line = gets().split(" ");
+let A = parseInt(line[0]);
+let B = parseInt(line[1]);
+let total = A+B;
+console.log("X= " +total);
+*/
+
+
+/*function redirecionar(){
+    window.open("https://www.netflix.com/browse");
+}
+function redirecionar1(){
+    window.open("https://www.google.com")
+}
+function redirecionar2(){
+    window.open("https://translate.google.com.br")
+}*/
+
+/*function validaIdade(idade){
+    var validar;
+    if (idade >=18){
+        validar = true
+    }else{
+        validar =false
+    }
+    return validar;
+}
+
+var idade = prompt("Qualsua idade");
+console.log(validaIdade(idade));*/
+
+/*
+var d = new Date();
+alert(d);*/
+
+
+/*
+var count;
+for(count= 0; count <=5; count++){
+    console.log(count);
+};*/
+
+
+
+
+
+/*var count = 0;
+while (count <=5){
+    console.log(count);
+    count++;
+};*/
+
+/*var idade = prompt ("Por favor, digite sua idade:");
+if (idade >= 18) {
+    alert ("Ok, pode continuar na página, voce é maior de idade");
+} else{
+    alert("Infelizmente voce terá que sair desta pagina pois é menor de idade");
+}*/
+
+/*
+var nome = "Marcos Guerreiro"
+var idade = 50
+alert("Esta página foi desenvolvida por " + nome + " que tem " + idade + " anos.");
+*/
