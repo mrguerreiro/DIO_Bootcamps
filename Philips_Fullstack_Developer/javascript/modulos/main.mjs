73ec0338acf8b358334be856ca06813fe8a98648
mrguerreiro <marcosguerreiro@hotmail.com>
2022-04-22 19:58:52 -0300

    Inclusao de aula sobre modulos

---
+++ Philips_Fullstack_Developer/javascript/modulos/main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby } from './funcoes.mjs';
+
+console.log(mostraIdade('Marcos', 51));
+console.log(mostraCidade('Marcos', 'Bauru'));
+console.log(mostraHobby('Marcos', 'codar'));
